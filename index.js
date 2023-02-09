7895875fd38a188d3d950dce0adb1e999594ab0a
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2023-02-09 13:57:09 +0200

    add dockerfile

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/status', (req, res) => res.status(200).json({status: 'ok'}))
+app.get('/status', (req, res) => res.status(200).json({status: 'oki'}))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
