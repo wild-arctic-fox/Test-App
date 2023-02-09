2fa6a2f5ada9163bd2daa3c0cbecb77c3600b90f
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2023-02-09 14:33:17 +0200

    update v3 (fixed)  jenkinsfile

--- script.groovy
+++ script.groovy
@@ -2,7 +2,7 @@ def buildImage() {
     echo 'building the docker image...'
     withCredentials([usernamePassword(credentialsId: 'my-dockerhub-repo', passwordVariable: 'PASSW', usernameVariable: 'USER')]) {
         sh 'docker build -t wildarcticfox/wild-private-repo:node-2.1 .'
-        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh "echo $PASSW | docker login -u $USER --password-stdin"
         sh 'docker push wildarcticfox/wild-private-repo:node-2.0'
     }
 }
