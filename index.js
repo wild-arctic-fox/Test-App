5bb1a5c782cf60025b3e6573ba5ffa6743f35097
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2023-02-10 21:33:39 +0200

    fuck

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 const express = require('express')
 const app = express()
 
-app.get('/status', (req, res) => res.status(200).json({status: 'oki test 333'}))
+app.get('/status', (req, res) => res.status(200).json({status: 'oki test 543'}))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
