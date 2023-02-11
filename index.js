be36d90de8aa1777fc39787ba48678fff32694cf
wild-arctic-fox <amusing.arctic.fox@gmail.com>
2023-02-11 12:03:45 +0200

    fix

--- index.js
+++
@@ -1,7 +0,0 @@
-
-const express = require('express')
-const app = express()
-
-app.get('/status', (req, res) => res.status(200).json({status: 'oki 887099 55'}))
-
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import * as express from 'express'
 import { Request, Response, Express } from 'express'
 const app: Express = express()
 
-app.get('/status', (req: Request, res: Response) => res.status(200).json({ status: 'oki typescript' }))
+app.get('/status', (req: Request, res: Response) => res.status(200).json({ status: req.headers }))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
