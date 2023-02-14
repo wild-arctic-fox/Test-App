8ba8c4bbe9c14619222ba6545bbdc2ec4b57bc7e
Olena Veprytska <amusing.arctic.fox@gmail.com>
2023-02-14 10:47:31 +0200

    update server

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ const app: Express = express()
 
 app.get('/status', (req: Request, res: Response) => res.status(200).json({ status: req.headers }))
 
-app.get('/', (req: Request, res: Response) => res.status(200).json({ status: 600 }))
+app.get('/', (req: Request, res: Response) => res.status(200).json({ status: req.ip }))
+
+app.get('/error', (req: Request, res: Response) => {throw new Error('Sth went wrong')})
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
